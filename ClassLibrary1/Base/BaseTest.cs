1d3c3c8f7fd4b7d84ccadb010fb9a8115fde99e1
Goldi Saini <goldisaini8101.gmail.com>
2019-01-20 14:52:21 +0530

    first commit

---
+++ ClassLibrary1/Base/BaseTest.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestApp.Base
+{
+    public class BaseTest
+    {
+       public  IWebDriver driver = null;
+
+        
+        public void setup(string driverName)
+        {
+            switch(driverName)
+            {
+                case "chrome":
+                    {
+                        driver = new ChromeDriver();
+                        break;
+                    }
+            }
+
+            driver.Manage().Window.Maximize();
+        }
+
+        [TearDown]
+        public void teardown()
+        {
+            driver.Close();
+        }
+
+        public void navigateToURL(string URL)
+        {
+            driver.Navigate().GoToUrl(URL);
+        }
+        public void waitHandler(int waitTime)
+        {
+            Thread.Sleep(waitTime);
+        }
+    }
+}
---
+++ ClassLibrary1/Base/Reusables.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace TestApp.Base
+{
+   public class Reusables
+    {
+
+        public void selectFromDropdown(IWebElement element,string value)
+        {
+            SelectElement select = new SelectElement(element);
+            select.SelectByText(value);
+        }
+
+        public bool IsElementDisplayed(IWebElement element)
+        {
+            try
+            {
+                return element.Displayed;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+
+           
+        }
+}
